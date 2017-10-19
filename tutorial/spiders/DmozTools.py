dc915d13d5798f0f148761d664e017bdd865a796
jian zhang <jian.zhang@fuwo.com>
2017-10-19 09:22:42 +0800

    modify parse of DmoztoolsSpider

--- tutorial/spiders/DmozTools.py
+++ tutorial/spiders/DmozTools.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from tutorial.items import TutorialItem
 
 
 class DmoztoolsSpider(scrapy.Spider):
@@ -9,6 +10,12 @@ class DmoztoolsSpider(scrapy.Spider):
                   'http://dmoztools.net/Computers/Programming/Languages/Python/Resources']
 
     def parse(self, response):
-        filename = response.url.split('/')[-2] + ".html"
-        with open(filename, 'wb') as fp:
-            fp.write(response.body)
+        # filename = response.url.split('/')[-2] + ".html"
+        # with open(filename, 'wb') as fp:
+        #     fp.write(response.body)
+        div_site_item = response.xpath('//*[@id="site-list-content"]/div')
+        for site_item in div_site_item:
+            item = TutorialItem()
+            item['title'] = site_item.css('div.title-and-desc div.site-title::text')
+            item['desc'] = site_item.css('div.title-and-desc div.site-descr::text')
+            item['link'] = site_item.css('div.title-and-desc a::attr(href)')
