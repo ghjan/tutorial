a74af3e8f3eb2f3c700f4cadfe19bc09d0ab3300
jian zhang <jian.zhang@fuwo.com>
2017-10-19 09:44:36 +0800

    use xpath instead of css;

--- tutorial/spiders/DmozTools.py
+++ tutorial/spiders/DmozTools.py
@@ -17,6 +17,7 @@ class DmoztoolsSpider(scrapy.Spider):
         for site_item in div_site_item:
             item = TutorialItem()
             item['title'] = site_item.css('div.title-and-desc div.site-title::text')
-            item['desc'] = site_item.css('div.title-and-desc div.site-descr::text')
+            # item['desc'] = site_item.css('div.title-and-desc div.site-descr::text')
+            item['desc'] = site_item.xpath('div.title-and-desc > div/text()')
             item['link'] = site_item.css('div.title-and-desc a::attr(href)')
             yield item
