87e04e006d21f263a7c737d090d5969b813a72be
David Zhang <cajan2@163.com>
2017-10-25 02:27:35 +0800

    fix a bug in middlewares.py

--- tutorial/middlewares.py
+++ tutorial/middlewares.py
@@ -20,35 +20,38 @@ GetIp_Single = GetIp()
 class ProxyMiddleware(object):
     http_n = 0  # counter for http requests
     https_n = 0  # counter for https requests
+    ips = {}
 
     # overwrite process request
     def process_request(self, request, spider):
+        if not self.ips:
+            self._get_ips()  # Set the location of the proxy
+        if request.url.startswith("http://"):
+            n = ProxyMiddleware.http_n
+            n = n if n < len(self.ips['http']) else 0
+            request.meta['proxy'] = "http://%s:%d" % (
+                self.ips['http'][n][0], int(self.ips['http'][n][1]))
+            logging.info('Squence - http: %s - %s' % (n, str(self.ips['http'][n])))
+            ProxyMiddleware.http_n = n + 1
+
+        if request.url.startswith("https://"):
+            n = ProxyMiddleware.https_n
+            n = n if n < len(self.ips['https']) else 0
+            request.meta['proxy'] = "https://%s:%d" % (
+                self.ips['https'][n][0], int(self.ips['https'][n][1]))
+            logging.info('Squence - https: %s - %s' % (n, str(self.ips['https'][n])))
+            ProxyMiddleware.https_n = n + 1
+
+    def _get_ips(self):
         count = 0
         timeout = 10
         while not GetIp_Single.result and timeout and count < timeout:
             time.sleep(1)
             count += 1
-        ips = {}
         if not GetIp_Single.result:
             print("use ipadd_backup!")
-            ips['http'] = [item.split(':') for item in IPPOOL_BACKUP_HTTP]
-            ips['https'] = [item.split(':') for item in IPPOOL_BACKUP_HTTPS]
+            self.ips['http'] = [item.split(':') for item in IPPOOL_BACKUP_HTTP]
+            self.ips['https'] = [item.split(':') for item in IPPOOL_BACKUP_HTTPS]
         else:
-            print("use result successfully!")
-            ips = GetIp().get_ips()
-        # Set the location of the proxy
-        if request.url.startswith("http://"):
-            n = ProxyMiddleware.http_n
-            n = n if n < len(ips['http']) else 0
-            request.meta['proxy'] = "http://%s:%d" % (
-                ips['http'][n][0], int(ips['http'][n][1]))
-            logging.info('Squence - http: %s - %s' % (n, str(ips['http'][n])))
-            ProxyMiddleware.http_n = n + 1
-
-        if request.url.startswith("https://"):
-            n = ProxyMiddleware.https_n
-            n = n if n < len(ips['https']) else 0
-            request.meta['proxy'] = "https://%s:%d" % (
-                ips['https'][n][0], int(ips['https'][n][1]))
-            logging.info('Squence - https: %s - %s' % (n, str(ips['https'][n])))
-            ProxyMiddleware.https_n = n + 1
+            print("use get_ips() result successfully!")
+            self.ips = GetIp().get_ips()
